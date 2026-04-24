8958eda544976f4db0f1d09f7f29603661a7b317
Vincent 张彬胜 <83479998+vincent-colin-tionando@users.noreply.github.com>
2026-04-24 16:05:00 +0700

    File awal
    Untuk menjalankan Laravel dan cache konfigurasi aplik

---
+++ bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Providers\AppServiceProvider;
+use App\Providers\AuthServiceProvider;  
+
+return [
+    AppServiceProvider::class,
+    AuthServiceProvider::class,          
+];
+
